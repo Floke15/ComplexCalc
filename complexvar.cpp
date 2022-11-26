0bdc8170f3bd25ccb16cd7ba6cd8af8b20381128
Florian Köttl <koettl@student.tugraz.at>
2022-11-26 16:15:17 +0100

    +added openGL-widget to complexvar *refactored complexvar constructor

--- complexvar.cpp
+++ complexvar.cpp
@@ -14,196 +14,203 @@ ComplexVar::ComplexVar(QWidget* parent, std::string name, std::complex<double> v
   color_(color)
 {
   this->setObjectName("newVariable");
-  this->setMinimumSize(200, 72);
+  this->setMinimumSize(200, 272); //72
   this->setFrameShape(QFrame::StyledPanel);
   //this->setMidLineWidth(1);
   QSizePolicy sizePolicy(QSizePolicy::MinimumExpanding, QSizePolicy::Fixed);
   sizePolicy.setHeightForWidth(this->sizePolicy().hasHeightForWidth());
-  
 
-  QVBoxLayout* verticalLayout = new QVBoxLayout(this);
-  verticalLayout->setContentsMargins(0, 0, 0, 0);
-  verticalLayout->setSpacing(0);
-  verticalLayout->setObjectName("VarVerticalLayout");
-  verticalLayout->setAlignment(Qt::AlignTop);
+  verticalLayout_ = new QVBoxLayout(this);
+  verticalLayout_->setContentsMargins(0, 0, 0, 0);
+  verticalLayout_->setSpacing(0);
+  verticalLayout_->setObjectName("VarVerticalLayout");
+  verticalLayout_->setAlignment(Qt::AlignTop);
 
 
-  QWidget* horizontalWidget1 = new QWidget(this);
-  horizontalWidget1->setObjectName("horizontalWidget1");
-  horizontalWidget1->setFixedHeight(24);
+  horizontalWidget1_ = new QWidget(this);
+  horizontalWidget1_->setObjectName("horizontalWidget1");
+  horizontalWidget1_->setFixedHeight(24);
   QSizePolicy sizePolicy4(QSizePolicy::MinimumExpanding, QSizePolicy::Fixed);
-  sizePolicy4.setHeightForWidth(horizontalWidget1->sizePolicy().hasHeightForWidth());
-  horizontalWidget1->setSizePolicy(sizePolicy4);
+  sizePolicy4.setHeightForWidth(horizontalWidget1_->sizePolicy().hasHeightForWidth());
+  horizontalWidget1_->setSizePolicy(sizePolicy4);
 
-  QWidget* horizontalWidget2 = new QWidget(this);
-  horizontalWidget2->setObjectName("horizontalWidget2");
-  horizontalWidget2->setFixedHeight(24);
-  horizontalWidget2->setSizePolicy(sizePolicy4);
+  horizontalWidget2_ = new QWidget(this);
+  horizontalWidget2_->setObjectName("horizontalWidget2");
+  horizontalWidget2_->setFixedHeight(24);
+  horizontalWidget2_->setSizePolicy(sizePolicy4);
 
-  QWidget* horizontalWidget3 = new QWidget(this);
-  horizontalWidget2->setObjectName("horizontalWidget2");
-  horizontalWidget2->setFixedHeight(24);
-  horizontalWidget2->setSizePolicy(sizePolicy4);
+  horizontalWidget3_ = new QWidget(this);
+  horizontalWidget3_->setObjectName("horizontalWidget3");
+  horizontalWidget3_->setFixedHeight(24);
+  horizontalWidget3_->setSizePolicy(sizePolicy4);
 
-  verticalLayout->addWidget(horizontalWidget1);
-  verticalLayout->addWidget(horizontalWidget2);
-  verticalLayout->addWidget(horizontalWidget3);
+  openGL3DWindow_ = new OpenGLWindow(false);
+  glWidget_ = QWidget::createWindowContainer(openGL3DWindow_);
+  QSizePolicy sizePolicy12(QSizePolicy::MinimumExpanding, QSizePolicy::Fixed);
+  glWidget_->setFixedHeight(200);
+  glWidget_->setObjectName("glWidget_");
+  glWidget_->setSizePolicy(sizePolicy12);
 
-
-  QHBoxLayout* horizontalLayout1 = new QHBoxLayout(horizontalWidget1);
-  horizontalLayout1->setContentsMargins(0, 0, 0, 0);
-  horizontalLayout1->setSpacing(0);
-  horizontalLayout1->setObjectName("horizontalLayout1");
-  horizontalLayout1->setAlignment(Qt::AlignLeft);
-
-  QHBoxLayout* horizontalLayout2 = new QHBoxLayout(horizontalWidget2);
-  horizontalLayout2->setContentsMargins(0, 0, 0, 0);
-  horizontalLayout2->setSpacing(0);
-  horizontalLayout2->setObjectName("horizontalLayout2");
-  horizontalLayout2->setAlignment(Qt::AlignLeft);
-
-  QHBoxLayout* horizontalLayout3 = new QHBoxLayout(horizontalWidget3);
-  horizontalLayout3->setContentsMargins(0, 0, 0, 0);
-  horizontalLayout3->setSpacing(0);
-  horizontalLayout3->setObjectName("horizontalLayout3");
-  horizontalLayout3->setAlignment(Qt::AlignLeft);
+  verticalLayout_->addWidget(horizontalWidget1_);
+  verticalLayout_->addWidget(horizontalWidget2_);
+  verticalLayout_->addWidget(horizontalWidget3_);
+  verticalLayout_->addWidget(glWidget_);
 
 
-  QPushButton* expandButton = new QPushButton(horizontalWidget1);
-  expandButton->setObjectName("expandButton");
-  expandButton->setText("Expand");
-  expandButton->setFixedHeight(24);
+  horizontalLayout1_ = new QHBoxLayout(horizontalWidget1_);
+  horizontalLayout1_->setContentsMargins(0, 0, 0, 0);
+  horizontalLayout1_->setSpacing(0);
+  horizontalLayout1_->setObjectName("horizontalLayout1");
+  horizontalLayout1_->setAlignment(Qt::AlignLeft);
+
+  horizontalLayout2_ = new QHBoxLayout(horizontalWidget2_);
+  horizontalLayout2_->setContentsMargins(0, 0, 0, 0);
+  horizontalLayout2_->setSpacing(0);
+  horizontalLayout2_->setObjectName("horizontalLayout2");
+  horizontalLayout2_->setAlignment(Qt::AlignLeft);
+
+  horizontalLayout3_ = new QHBoxLayout(horizontalWidget3_);
+  horizontalLayout3_->setContentsMargins(0, 0, 0, 0);
+  horizontalLayout3_->setSpacing(0);
+  horizontalLayout3_->setObjectName("horizontalLayout3");
+  horizontalLayout3_->setAlignment(Qt::AlignLeft);
+
+
+  expandButton_ = new QPushButton(horizontalWidget1_);
+  expandButton_->setObjectName("expandButton");
+  expandButton_->setText("Expand");
+  expandButton_->setFixedHeight(24);
   QSizePolicy sizePolicy1(QSizePolicy::MinimumExpanding, QSizePolicy::Fixed);
   sizePolicy1.setHorizontalStretch(1);
-  sizePolicy1.setHeightForWidth(expandButton->sizePolicy().hasHeightForWidth());
-  expandButton->setSizePolicy(sizePolicy1);
+  sizePolicy1.setHeightForWidth(expandButton_->sizePolicy().hasHeightForWidth());
+  expandButton_->setSizePolicy(sizePolicy1);
 
-  QPushButton* switchButton = new QPushButton(horizontalWidget1);
-  switchButton->setObjectName("switchButton");
-  switchButton->setText("Euler");
-  switchButton->setFixedHeight(24);
-  switchButton->setSizePolicy(sizePolicy1);
+  switchButton_ = new QPushButton(horizontalWidget1_);
+  switchButton_->setObjectName("switchButton");
+  switchButton_->setText("Euler");
+  switchButton_->setFixedHeight(24);
+  switchButton_->setSizePolicy(sizePolicy1);
 
-  QPushButton* deleteButton = new QPushButton(horizontalWidget1);
-  deleteButton->setObjectName("deleteButton");
-  deleteButton->setText("-");
-  deleteButton->setFixedSize(20, 20);
-  deleteButton->setStyleSheet("border: 1px; background-color: rgb(196, 43, 28)");
+  deleteButton_ = new QPushButton(horizontalWidget1_);
+  deleteButton_->setObjectName("deleteButton");
+  deleteButton_->setText("X");
+  deleteButton_->setFixedSize(20, 20);
+  deleteButton_->setStyleSheet("border: 1px; background-color: rgb(196, 43, 28)");
   QSizePolicy sizePolicy5(QSizePolicy::Fixed, QSizePolicy::Fixed);
   sizePolicy5.setHorizontalStretch(0);
-  sizePolicy5.setHeightForWidth(deleteButton->sizePolicy().hasHeightForWidth());
-  deleteButton->setSizePolicy(sizePolicy5);
+  sizePolicy5.setHeightForWidth(deleteButton_->sizePolicy().hasHeightForWidth());
+  deleteButton_->setSizePolicy(sizePolicy5);
 
-  horizontalLayout1->addWidget(expandButton);
-  horizontalLayout1->addWidget(switchButton);
-  horizontalLayout1->addWidget(deleteButton);
+  horizontalLayout1_->addWidget(expandButton_);
+  horizontalLayout1_->addWidget(switchButton_);
+  horizontalLayout1_->addWidget(deleteButton_);
 
 
-  QLineEdit* nameInput = new QLineEdit(this);
-  nameInput->setObjectName("nameInput");
-  nameInput->setFixedHeight(24);
-  nameInput->setPlaceholderText("Name");
+  nameInput_ = new QLineEdit(this);
+  nameInput_->setObjectName("nameInput");
+  nameInput_->setFixedHeight(24);
+  nameInput_->setPlaceholderText("Name");
   QSizePolicy sizePolicy2(QSizePolicy::MinimumExpanding, QSizePolicy::Fixed);
   sizePolicy2.setHorizontalStretch(1);
-  sizePolicy2.setHeightForWidth(nameInput->sizePolicy().hasHeightForWidth());
-  nameInput->setSizePolicy(sizePolicy2);
+  sizePolicy2.setHeightForWidth(nameInput_->sizePolicy().hasHeightForWidth());
+  nameInput_->setSizePolicy(sizePolicy2);
 
-  QWidget* omegaSubWidget = new QWidget();
-  omegaSubWidget->setObjectName("omegaSubWidget");
-  omegaSubWidget->setFixedHeight(24);
+  omegaSubWidget_ = new QWidget();
+  omegaSubWidget_->setObjectName("omegaSubWidget");
+  omegaSubWidget_->setFixedHeight(24);
   QSizePolicy sizePolicy6(QSizePolicy::MinimumExpanding, QSizePolicy::Fixed);
   sizePolicy6.setHorizontalStretch(1);
-  sizePolicy6.setHeightForWidth(omegaSubWidget->sizePolicy().hasHeightForWidth());
-  omegaSubWidget->setSizePolicy(sizePolicy6);
+  sizePolicy6.setHeightForWidth(omegaSubWidget_->sizePolicy().hasHeightForWidth());
+  omegaSubWidget_->setSizePolicy(sizePolicy6);
 
-  horizontalLayout2->addWidget(nameInput);
-  horizontalLayout2->addWidget(omegaSubWidget);
+  horizontalLayout2_->addWidget(nameInput_);
+  horizontalLayout2_->addWidget(omegaSubWidget_);
 
-  QHBoxLayout* horizontalLayoutOmega = new QHBoxLayout(omegaSubWidget);
-  horizontalLayoutOmega->setContentsMargins(0, 0, 0, 0);
-  horizontalLayoutOmega->setSpacing(0);
-  horizontalLayoutOmega->setObjectName("omegaSubWidget");
-  horizontalLayoutOmega->setAlignment(Qt::AlignLeft);
+  horizontalLayoutOmega_ = new QHBoxLayout(omegaSubWidget_);
+  horizontalLayoutOmega_->setContentsMargins(0, 0, 0, 0);
+  horizontalLayoutOmega_->setSpacing(0);
+  horizontalLayoutOmega_->setObjectName("omegaSubWidget");
+  horizontalLayoutOmega_->setAlignment(Qt::AlignLeft);
 
-  QLabel* omegaLabel = new QLabel(omegaSubWidget);
-  omegaLabel->setObjectName("omegaLabel");
-  omegaLabel->setFixedSize(20, 24);
-  omegaLabel->setText("ω:");
-  omegaLabel->setAlignment(Qt::AlignRight | Qt::AlignVCenter);
+  omegaLabel_ = new QLabel(omegaSubWidget_);
+  omegaLabel_->setObjectName("omegaLabel");
+  omegaLabel_->setFixedSize(20, 24);
+  omegaLabel_->setText("ω:");
+  omegaLabel_->setAlignment(Qt::AlignRight | Qt::AlignVCenter);
   QSizePolicy sizePolicy7(QSizePolicy::Fixed, QSizePolicy::Fixed);
-  sizePolicy7.setHeightForWidth(omegaLabel->sizePolicy().hasHeightForWidth());
-  omegaLabel->setSizePolicy(sizePolicy7);
+  sizePolicy7.setHeightForWidth(omegaLabel_->sizePolicy().hasHeightForWidth());
+  omegaLabel_->setSizePolicy(sizePolicy7);
 
-  QLineEdit* omegaInput = new QLineEdit(this);
-  omegaInput->setObjectName("nameInput");
-  omegaInput->setFixedHeight(24);
+  omegaInput_ = new QLineEdit(this);
+  omegaInput_->setObjectName("nameInput");
+  omegaInput_->setFixedHeight(24);
   QSizePolicy sizePolicy8(QSizePolicy::MinimumExpanding, QSizePolicy::Fixed);
-  sizePolicy8.setHeightForWidth(nameInput->sizePolicy().hasHeightForWidth());
-  omegaInput->setSizePolicy(sizePolicy8);
+  sizePolicy8.setHeightForWidth(nameInput_->sizePolicy().hasHeightForWidth());
+  omegaInput_->setSizePolicy(sizePolicy8);
 
-  horizontalLayoutOmega->addWidget(omegaLabel);
-  horizontalLayoutOmega->addWidget(omegaInput);
+  horizontalLayoutOmega_->addWidget(omegaLabel_);
+  horizontalLayoutOmega_->addWidget(omegaInput_);
 
   
-  QWidget* varSubWidget1 = new QWidget();
-  varSubWidget1->setObjectName("varSubWidget1");
-  varSubWidget1->setFixedHeight(24);
+  varSubWidget1_ = new QWidget();
+  varSubWidget1_->setObjectName("varSubWidget1");
+  varSubWidget1_->setFixedHeight(24);
   QSizePolicy sizePolicy10(QSizePolicy::MinimumExpanding, QSizePolicy::Fixed);
   sizePolicy10.setHorizontalStretch(1);
-  sizePolicy10.setHeightForWidth(varSubWidget1->sizePolicy().hasHeightForWidth());
-  varSubWidget1->setSizePolicy(sizePolicy10);
+  sizePolicy10.setHeightForWidth(varSubWidget1_->sizePolicy().hasHeightForWidth());
+  varSubWidget1_->setSizePolicy(sizePolicy10);
 
-  QWidget* varSubWidget2 = new QWidget();
-  varSubWidget2->setObjectName("varSubWidget2");
-  varSubWidget2->setFixedHeight(24);
-  varSubWidget2->setSizePolicy(sizePolicy10);
+  varSubWidget2_ = new QWidget();
+  varSubWidget2_->setObjectName("varSubWidget2");
+  varSubWidget2_->setFixedHeight(24);
+  varSubWidget2_->setSizePolicy(sizePolicy10);
 
-  horizontalLayout3->addWidget(varSubWidget1);
-  horizontalLayout3->addWidget(varSubWidget2);
+  horizontalLayout3_->addWidget(varSubWidget1_);
+  horizontalLayout3_->addWidget(varSubWidget2_);
 
-  QHBoxLayout* horizontalLayoutInput1 = new QHBoxLayout(varSubWidget1);
-  horizontalLayoutInput1->setContentsMargins(0, 0, 0, 0);
-  horizontalLayoutInput1->setSpacing(0);
-  horizontalLayoutInput1->setObjectName("horizontalLayoutInput1");
-  horizontalLayoutInput1->setAlignment(Qt::AlignLeft);
+  horizontalLayoutInput1_ = new QHBoxLayout(varSubWidget1_);
+  horizontalLayoutInput1_->setContentsMargins(0, 0, 0, 0);
+  horizontalLayoutInput1_->setSpacing(0);
+  horizontalLayoutInput1_->setObjectName("horizontalLayoutInput1");
+  horizontalLayoutInput1_->setAlignment(Qt::AlignLeft);
 
-  QLabel* input1Label = new QLabel(this);
-  input1Label->setObjectName("input1Label");
-  input1Label->setFixedSize(20, 24);
-  input1Label->setText("Re:");
-  input1Label->setAlignment(Qt::AlignRight | Qt::AlignVCenter);
+  input1Label_ = new QLabel(this);
+  input1Label_->setObjectName("input1Label");
+  input1Label_->setFixedSize(20, 24);
+  input1Label_->setText("Re:");
+  input1Label_->setAlignment(Qt::AlignRight | Qt::AlignVCenter);
   QSizePolicy sizePolicy3(QSizePolicy::Fixed, QSizePolicy::Fixed);
-  sizePolicy3.setHeightForWidth(nameInput->sizePolicy().hasHeightForWidth());
-  input1Label->setSizePolicy(sizePolicy3);
+  sizePolicy3.setHeightForWidth(nameInput_->sizePolicy().hasHeightForWidth());
+  input1Label_->setSizePolicy(sizePolicy3);
 
-  QLineEdit* input1Input = new QLineEdit(this);
-  input1Input->setObjectName("input1Input");
-  input1Input->setFixedHeight(24);
+  input1Input_ = new QLineEdit(this);
+  input1Input_->setObjectName("input1Input");
+  input1Input_->setFixedHeight(24);
   QSizePolicy sizePolicy11(QSizePolicy::MinimumExpanding, QSizePolicy::Fixed);
-  sizePolicy11.setHeightForWidth(input1Input->sizePolicy().hasHeightForWidth());
-  input1Input->setSizePolicy(sizePolicy11);
+  sizePolicy11.setHeightForWidth(input1Input_->sizePolicy().hasHeightForWidth());
+  input1Input_->setSizePolicy(sizePolicy11);
 
-  horizontalLayoutInput1->addWidget(input1Label);
-  horizontalLayoutInput1->addWidget(input1Input);
+  horizontalLayoutInput1_->addWidget(input1Label_);
+  horizontalLayoutInput1_->addWidget(input1Input_);
 
-  QHBoxLayout* horizontalLayoutInput2 = new QHBoxLayout(varSubWidget2);
-  horizontalLayoutInput2->setContentsMargins(0, 0, 0, 0);
-  horizontalLayoutInput2->setSpacing(0);
-  horizontalLayoutInput2->setObjectName("horizontalLayoutInput2");
-  horizontalLayoutInput2->setAlignment(Qt::AlignLeft);
+  horizontalLayoutInput2_ = new QHBoxLayout(varSubWidget2_);
+  horizontalLayoutInput2_->setContentsMargins(0, 0, 0, 0);
+  horizontalLayoutInput2_->setSpacing(0);
+  horizontalLayoutInput2_->setObjectName("horizontalLayoutInput2");
+  horizontalLayoutInput2_->setAlignment(Qt::AlignLeft);
 
-  QLabel* input2Label = new QLabel(this);
-  input2Label->setObjectName("input2Label");
-  input2Label->setFixedSize(20, 24);
-  input2Label->setText("Im:");
-  input2Label->setAlignment(Qt::AlignRight | Qt::AlignVCenter);
-  input1Label->setSizePolicy(sizePolicy3);
+  input2Label_ = new QLabel(this);
+  input2Label_->setObjectName("input2Label");
+  input2Label_->setFixedSize(20, 24);
+  input2Label_->setText("Im:");
+  input2Label_->setAlignment(Qt::AlignRight | Qt::AlignVCenter);
+  input1Label_->setSizePolicy(sizePolicy3);
 
-  QLineEdit* input2Input = new QLineEdit(this);
-  input2Input->setObjectName("input2Input");
-  input2Input->setFixedHeight(24);
-  input1Input->setSizePolicy(sizePolicy11);
+  input2Input_ = new QLineEdit(this);
+  input2Input_->setObjectName("input2Input");
+  input2Input_->setFixedHeight(24);
+  input1Input_->setSizePolicy(sizePolicy11);
 
-  horizontalLayoutInput2->addWidget(input2Label);
-  horizontalLayoutInput2->addWidget(input2Input);
+  horizontalLayoutInput2_->addWidget(input2Label_);
+  horizontalLayoutInput2_->addWidget(input2Input_);
 }
